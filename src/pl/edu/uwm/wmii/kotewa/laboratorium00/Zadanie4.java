6c29e3c68be71ad31e9c7b0815fc15898fe51952
Patryk Truchan <162391@student.uwm.edu.pl>
2021-10-12 09:11:57 +0200

    Initial commit

---
+++ src/pl/edu/uwm/wmii/kotewa/laboratorium00/Zadanie4.java
@@ -0,0 +1,13 @@
+package pl.edu.uwm.wmii.kotewa.laboratorium00;
+
+public class Zadanie4 {
+
+    public static void main(String[] args) {
+        int wynik;
+        wynik=1000;
+        System .out. println (wynik);
+        System .out. println (wynik+wynik*6/100);
+        wynik=wynik+wynik*6/100;
+        System .out. println (wynik+wynik*6/100);
+    }
+}
